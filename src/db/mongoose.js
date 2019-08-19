86bf87686f49b01c7f611b97edd6b570a874d144
Jin Kim <Limbomaster7@gmail.com>
2019-08-19 22:17:51 +0900

    1

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true, 
     useCreateIndex: true
 })
